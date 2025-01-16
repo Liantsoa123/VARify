e02206536bb899bbaff8928a7e5b760b09d3fa98
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-01-16 07:00:57 +0300

    initial commit

---
+++ VARify/Form1.Designer.cs
@@ -0,0 +1,14 @@
+﻿using OpenCvSharp;
+
+using VARify.model;
+using EventArgs = System.EventArgs;
+using Point = OpenCvSharp.Point;
+
+namespace VARify;
+
+partial class Form1
+{
+    
+    
+}
+    
---
+++ VARify/Form1.cs
@@ -0,0 +1,9 @@
+namespace VARify;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
